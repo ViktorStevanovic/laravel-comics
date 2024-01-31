21c5f9caed00c684bbe331ddf6be509c9b63231b
ViktorStevanovic <stevanovic.lavoro@gmail.com>
2024-01-31 16:10:02 +0100

    added and styled button

--- resources/views/pages/comics.blade.php
+++ resources/views/pages/comics.blade.php
@@ -5,7 +5,7 @@
     <section class="jumbotron">
         <img src="{{Vite::asset('resources/img/jumbotron.jpg')}}" alt="">
     </section>
-    <section class="comics-section container pt-5">
+    <section class="comics-section container pt-5 d-flex flex-column align-items-center">
         <div class="row">
             @foreach ($comics as $comic)
             <div class="col-2 d-flex align-items-stretch pb-4">
@@ -16,6 +16,7 @@
             </div>
             @endforeach
         </div>
+        <button>Load more</button>
     </section>
 </div>
 @endsection
